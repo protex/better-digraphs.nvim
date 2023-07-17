d7e4af635b60b1397444f8f1227f0bf5fde557f9
Tristan Brugère <67050900+tbrugere@users.noreply.github.com>
2023-07-17 11:01:52 -0700

    corrected length check for the digraph to support utf8 characters (more than 1 byte, but only 1 character)

--- lua/better-digraphs/init.lua
+++ lua/better-digraphs/init.lua
@@ -128,7 +128,7 @@ if vim.g.BetterDigraphsAdditions then
     if string.len(digraph_addition.digraph) ~= 2 then
       error('Digraph ' .. digraph_addition.digraph .. ' should have 2 characters, found ' .. string.len(digraph_addition.digraph))
     end
-    if string.len(digraph_addition.symbol) ~= 1 then
+    if vim.fn.strdisplaywidth(digraph_addition.symbol) ~= 1 then
       error('Digraph symbol ' .. digraph_addition.symbol .. ' should have 1 characters, found ' .. string.len(digraph_addition.symbol))
     end
     default_mapped_by_digraph[digraph_addition.digraph] = {
